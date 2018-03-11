113700aaecb7beb37afc73558532fd49c0cb5eff
Olcina <karlos.olcina@gmail.com>
2018-03-11 20:52:55 +0100

    chore: added yarn and express

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+// root page
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname +'/index.html'));
+})
+// serving all files trought static file
+app.use(express.static(__dirname))
+app.listen(8000)
+console.log("Restaurant app running at port 8000");

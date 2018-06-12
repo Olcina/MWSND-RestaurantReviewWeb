51999e4414848d68bec2557fc970b214a32d301e
Olcina <karlos.olcina@gmail.com>
2018-06-12 19:40:00 +0200

    feat: service worker add to the cache web assets
    closes #5

--- sw.js
+++ sw.js
@@ -1,7 +1,31 @@
 
+const cache_name = 'restaurant-cache-v1'
 
-console.log('sw')
+self.addEventListener('install', function (event) {
+    let urlsToCache = [
+        '/css/styles.css',
+        '/index.html'
+    ]
+
+    event.waitUntil(
+        caches.open(cache_name).then(cache =>{
+           return cache.addAll(urlsToCache);
+        })
+    )
+})
+
     
-self.addEventListener('fetch', function(event) {
-        console.log('fetching', event.request)
-    });
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        // match the request in any cache stored
+        caches.match(event.request).then(function (response) {
+                // if there is a match return it || fetch the response and add to the cache
+                return response || fetch(event.request).then(function (response) {
+                    return caches.open(cache_name).then(function(cache){
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                });
+        })
+    );
+});

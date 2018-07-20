2897404a931a8704b469b8b065ec3e8709097c05
Olcina <karlos.olcina@gmail.com>
2018-07-20 10:38:51 +0200

    feat: user information toast for connectivity

--- js/review_form.js
+++ js/review_form.js
@@ -148,6 +148,13 @@ form.onsubmit = function (event) {
         "rating": rating_value,
         "comments": review_text
     }
+
+    // check connectivity
+    if (navigator.onLine) {
+        console.log('online');
+    } else {
+        console.log('offline');
+    }
     
     postData(`http://localhost:1337/reviews/`, data)
         .then(data => {
@@ -171,4 +178,30 @@ form.onsubmit = function (event) {
 
 function postReviewtoIDB(data) {
     
+}
+
+// script from https://www.w3schools.com/howto/howto_js_snackbar.asp
+
+window.addEventListener('offline', function (e) {
+
+    activateToast('toast', 'Connection with the server was lost')
+
+});
+
+window.addEventListener('online', function (e) { 
+
+    activateToast('toast','Connection with the server was restablished')
+     
+});
+
+function activateToast(html_id, message) {
+
+    var toast = document.getElementById(html_id);
+
+    // Add the "show" class to DIV
+    toast.innerText = message
+    toast.className = "toast show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 3000);
 }
